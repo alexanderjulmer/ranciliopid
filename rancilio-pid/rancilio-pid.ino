051144451f18032930477bcaf8d655f578a83ed5
Miau09 <50794912+Miau09@users.noreply.github.com>
2020-06-06 12:35:46 +0200

    Update rancilio-pid.ino
    update screen layout

--- rancilio-pid/rancilio-pid.ino
+++ rancilio-pid/rancilio-pid.ino
@@ -1000,79 +1000,52 @@ void printScreen() {
     previousMillisDisplay = currentMillisDisplay;
     if (!sensorError) {
       u8g2.clearBuffer();
-      u8g2.drawXBMP(0, 0, logo_width, logo_height, logo_bits_u8g2);   //draw temp icon
-      u8g2.setCursor(32, 14);
-      u8g2.print("Ist :  ");
-      u8g2.print(Input, 1);
-      u8g2.print(" ");
-      u8g2.print((char)176);
-      u8g2.print("C");
-      u8g2.setCursor(32, 24);
-      u8g2.print("Soll:  ");
-      u8g2.print(setPoint, 1);
-      u8g2.print(" ");
-      u8g2.print((char)176);
-      u8g2.print("C");
-
+      //draw outline frame
+      //u8g2.drawFrame(0, 0, 128, 64);
       // Draw heat bar outline
-      u8g2.drawLine(15, 58, 117, 58);
-      u8g2.drawLine(15, 58, 15, 61);
-      u8g2.drawLine(117, 58, 117, 61);
-
+      u8g2.drawFrame(15, 58, 102, 4);
       u8g2.drawLine(16, 59, (Output / 10) + 16, 59);
       u8g2.drawLine(16, 60, (Output / 10) + 16, 60);
-      u8g2.drawLine(15, 61, 117, 61);
 
-      //draw current temp in icon
-      if (fabs(Input  - setPoint) < 0.3) {
+      //draw (blinking) temp
+      if (fabs(Input - setPoint) < 0.3) {
         if (isrCounter < 500) {
-          u8g2.drawLine(9, 48, 9, 58 - (Input  / 2));
-          u8g2.drawLine(10, 48, 10, 58 - (Input  / 2));
-          u8g2.drawLine(11, 48, 11, 58 - (Input  / 2));
-          u8g2.drawLine(12, 48, 12, 58 - (Input  / 2));
-          u8g2.drawLine(13, 48, 13, 58 - (Input  / 2));
+          u8g2.setCursor(2, 2);
+          u8g2.setFont(u8g2_font_profont22_tf);
+          u8g2.print(Input, 1);
+          u8g2.setFont(u8g2_font_open_iconic_arrow_2x_t);
+          u8g2.print(char(78));
+          u8g2.setCursor(78, 2);
+          u8g2.setFont(u8g2_font_profont22_tf);
+          u8g2.print(setPoint, 1);
         }
-      } else if (Input > 106) {
-        u8g2.drawLine(9, 48, 9, 5);
-        u8g2.drawLine(10, 48, 10, 4);
-        u8g2.drawLine(11, 48, 11, 3);
-        u8g2.drawLine(12, 48, 12, 4);
-        u8g2.drawLine(13, 48, 13, 5);
       } else {
-        u8g2.drawLine(9, 48, 9, 58 - (Input  / 2));
-        u8g2.drawLine(10, 48, 10, 58 - (Input  / 2));
-        u8g2.drawLine(11, 48, 11, 58 - (Input  / 2));
-        u8g2.drawLine(12, 48, 12, 58 - (Input  / 2));
-        u8g2.drawLine(13, 48, 13, 58 - (Input  / 2));
+        u8g2.setCursor(2, 2);
+        u8g2.setFont(u8g2_font_profont22_tf);
+        u8g2.print(Input, 1);
+        u8g2.setFont(u8g2_font_open_iconic_arrow_2x_t);
+        u8g2.setCursor(56, 6);
+        if (pidMode == 1) {
+          u8g2.print(char(74));
+        } else {
+          u8g2.print(char(70));
+        }
+        u8g2.setCursor(79, 2);
+        u8g2.setFont(u8g2_font_profont22_tf);
+        u8g2.print(setPoint, 1);
       }
 
-      //draw setPoint line
-      u8g2.drawLine(18, 58 - (setPoint / 2), 23, 58 - (setPoint / 2));
 
-      // PID Werte ueber heatbar
-      u8g2.setCursor(40, 48);
-
-      u8g2.print(bPID.GetKp(), 0); // P
-      u8g2.print("|");
-      if (bPID.GetKi() != 0) {
-        u8g2.print(bPID.GetKp() / bPID.GetKi(), 0);;
-      } // I
-      else
-      {
-        u8g2.print("0");
-      }
-      u8g2.print("|");
-      u8g2.print(bPID.GetKd() / bPID.GetKp(), 0); // D
-      u8g2.setCursor(98, 48);
-      if (Output < 99) {
-        u8g2.print(Output / 10, 1);
+      if (brewcounter > 10) {
+        u8g2.setFont(u8g2_font_profont17_tf);
+        // Brew
+        u8g2.setCursor(2, 30);
       } else {
-        u8g2.print(Output / 10, 0);
+        u8g2.setFont(u8g2_font_profont10_tf);
+        // Brew
+        u8g2.setCursor(36, 30);
       }
-      u8g2.print("%");
 
-      // Brew
-      u8g2.setCursor(32, 34);
       u8g2.print("Brew:  ");
       u8g2.print(bezugsZeit / 1000, 1);
       u8g2.print("/");
@@ -1081,43 +1054,35 @@ void printScreen() {
       }
       else
       {
-        u8g2.print(totalbrewtime / 1000);            // aktivieren wenn Preinfusion
+        u8g2.print(totalbrewtime / 1000, 0);           // aktivieren wenn Preinfusion
       }
-      //draw box
-      u8g2.drawFrame(0, 0, 128, 64);
 
-      // Für Statusinfos
-      u8g2.drawFrame(32, 0, 84, 12);
+      if (timerBrewdetection == 1 && brewcounter == 10) {
+        u8g2.setFont(u8g2_font_profont11_tf);
+        // Brew
+        u8g2.setCursor(30, 40);
+        u8g2.print("BD:  ");
+        u8g2.print((millis() - timeBrewdetection) / 1000, 1);
+        u8g2.print("/");
+        u8g2.print(brewtimersoftware, 0);
+      }
+
+      // Für Statusinfos       
       if (Offlinemodus == 0) {
         getSignalStrength();
-        if (WiFi.status() == WL_CONNECTED) {
-          u8g2.drawXBMP(40, 2, 8, 8, antenna_OK_u8g2);
-          for (int b = 0; b <= bars; b++) {
-            u8g2.drawVLine(45 + (b * 2), 10 - (b * 2), b * 2);
-          }
+        if (WiFi.status() != WL_CONNECTED) {
+          u8g2.drawFrame(116, 28, 12, 12);
+          u8g2.drawXBMP(118, 30, 8, 8, antenna_NOK_u8g2);
         } else {
-          u8g2.drawXBMP(40, 2, 8, 8, antenna_NOK_u8g2);
-          u8g2.setCursor(88, 2);
-          u8g2.print("RC: ");
-          u8g2.print(wifiReconnects);
-        }
-        if (Blynk.connected()) {
-          u8g2.drawXBMP(60, 2, 11, 8, blynk_OK_u8g2);
-        } else {
-          u8g2.drawXBMP(60, 2, 8, 8, blynk_NOK_u8g2);
-        }
-        if (MQTT == 1) {
-          if (client.connect("arduino", "try", "try")) {
-            u8g2.setCursor(77, 2);
-            u8g2.print("MQTT");
-          } else {
-            u8g2.setCursor(77, 2);
-            u8g2.print("");
+          if (!Blynk.connected()) {
+            u8g2.drawFrame(116, 28, 12, 12);
+            u8g2.drawXBMP(118, 30, 8, 8, blynk_NOK_u8g2);
           }
         }
       } else {
-        u8g2.setCursor(40, 2);
-        u8g2.print("Offlinemodus");
+        u8g2.drawFrame(116, 28, 12, 12);
+        u8g2.setCursor(120, 30);
+        u8g2.print("O");
       }
       u8g2.sendBuffer();
     }
